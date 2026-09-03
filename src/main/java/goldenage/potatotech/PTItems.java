faeaf2e29f185f2eed7451d2cee75ae669bb097e
MidFord <71330862+TheMidFord@users.noreply.github.com>
2026-09-03 06:09:32 +0200

    Overhauled the chute model, fixed some broken stuff.

--- src/main/java/goldenage/potatotech/PTItems.java
+++ src/main/java/goldenage/potatotech/PTItems.java
@@ -33,32 +33,32 @@ public class PTItems {
 
 		int id = config.getInt("starting_item_id");
 
-		potato = simpleItem("potato", "potato", "potato", id++, CreativeInventoryCategory.FOOD);
-		wrench = customItem(new ItemWrench("wrench", NamespaceID.getPermanent(MOD_ID, "wrench"), id++), "wrench", CreativeInventoryCategory.TOOLS);
-		crushedIronOre = simpleItem("crushed iron ore", "crushed_iron_ore", "crushed_iron_ore", id++, CreativeInventoryCategory.ORE_PRODUCTS);
-		crushedGoldOre = simpleItem("crushed gold ore", "crushed_gold_ore", "crushed_gold_ore", id++, CreativeInventoryCategory.ORE_PRODUCTS);
-		energyConnector = customItem(new ItemPlaceable(NamespaceID.getPermanent(MOD_ID, "item_energy_connector"), "energy_connector", id++, PTBlocks.energyConnector), "item_connector", CreativeInventoryCategory.REDSTONE);
-		wireSpool = customItem(new ItemWireSpool("wire_spool", NamespaceID.getPermanent(MOD_ID, "wire_spool"), id++), "wire_spool", CreativeInventoryCategory.REDSTONE);
-		electricHeatingUnit = simpleItem("Electric Heating Unit", "electric_heating_unit", "electric_heating_unit", id++, CreativeInventoryCategory.REDSTONE).setMaxStackSize(4);
-		redstoneIronMix = simpleItem("Redstone IronMix", "redstone_iron_mix", "redstone_iron_mix", id++, CreativeInventoryCategory.REDSTONE);
-		redstoneAlloy = simpleItem("Redstone Allow", "redstone_alloy", "redstone_alloy", id++, CreativeInventoryCategory.REDSTONE);
+		potato = simpleItem("potato", "potato", "potato", id++);
+		wrench = customItem(new ItemWrench("wrench", new NamespaceID(MOD_ID, "wrench"), id++), "wrench");
+		crushedIronOre = simpleItem("crushed_iron_ore", "crushed_iron_ore", "crushed_iron_ore", id++);
+		crushedGoldOre = simpleItem("crushed_gold_ore", "crushed_gold_ore", "crushed_gold_ore", id++);
+		energyConnector = customItem(new ItemPlaceable(new NamespaceID(MOD_ID, "item_energy_connector"), "energy_connector", id++, PTBlocks.energyConnector), "item_connector");
+		wireSpool = customItem(new ItemWireSpool("wire_spool", new NamespaceID(MOD_ID, "wire_spool"), id++), "wire_spool");
+		electricHeatingUnit = simpleItem("electric_heating_unit", "electric_heating_unit", "electric_heating_unit", id++).setMaxStackSize(4);
+		redstoneIronMix = simpleItem("redstone_iron_mix", "redstone_iron_mix", "redstone_iron_mix", id++);
+		redstoneAlloy = simpleItem("redstone_alloy", "redstone_alloy", "redstone_alloy", id++);
 	}
 
 
-	public static Item customItem(Item item, String texture, CreativeInventoryCategory category) {
+	public static Item customItem(Item item, String texture) {
 		itemTextures.put(item, texture);
-		LOGGER.info("Registering Custom item '{}' with texture 'potatotech:item/{}'.", item.namespaceID.toString(), texture);
+		LOGGER.info("Registering Custom item '{}' with texture 'potatotech:item/{}'.", item.namespaceID, texture);
 		return new ItemBuilder(MOD_ID)
-			.setCreativeInventoryPlacement(new CreativeInventoryPlacement.Category(category))
+			.setCreativeInventoryPlacement(new CreativeInventoryPlacement.Category(CreativeInventoryCategory.MISCELLANEOUS))
 			.build(item);
 	}
 
-	public static Item simpleItem(String name, String lang, String texture, int id, CreativeInventoryCategory category) {
-		Item item = new Item(NamespaceID.getPermanent(MOD_ID, name), lang, id);
+	public static Item simpleItem(String name, String lang, String texture, int id) {
+		Item item = new Item(new NamespaceID(MOD_ID, name), lang, id);
 		itemTextures.put(item, texture);
-		LOGGER.info("Registering item '{}' with texture 'potatotech:item/{}'.", item.namespaceID.toString(), texture);
+		LOGGER.info("Registering item '{}' with texture 'potatotech:item/{}'.", item.namespaceID, texture);
 		return new ItemBuilder(MOD_ID)
-			.setCreativeInventoryPlacement(new CreativeInventoryPlacement.Category(category))
+			.setCreativeInventoryPlacement(new CreativeInventoryPlacement.Category(CreativeInventoryCategory.MISCELLANEOUS))
 			.build(item);
 	}
 }
